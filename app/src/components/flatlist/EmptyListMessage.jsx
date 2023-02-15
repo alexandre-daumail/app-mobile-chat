0ee374c704aaf6d149c39fefef1fb0e48703f363
alexandre-daumail <alexandre.daumail@laplateforme.io>
2023-02-15 10:23:53 +0100

    EmplyListMessage component added

---
+++ app/src/components/flatlist/EmptyListMessage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function EmptyListMessage() {
+  return (
+    <Text>Aucun groupes existants</Text>
+  )
+}
+
+export default EmptyListMessage
